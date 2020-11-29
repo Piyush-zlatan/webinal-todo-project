38b87b2e523a29a4c1ad951b3f1ecadd19b4b817
Piyush Agarwal <piyush.agarwal@bankaks.com>
2020-11-29 14:51:50 +0530

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,15 @@
+//add an eventListener to the from
+const form = document.querySelector('#itemForm'); // select form
+const itemInput = document.querySelector('#itemInput'); // select input box from form
+const itemList = document.querySelector('.item-list');
+const feedback = document.querySelector('.feedback');
+const clearButton = document.querySelector('#clear-list');
+
+let todoItems = [];
+
+
+
+
+
+  
+
